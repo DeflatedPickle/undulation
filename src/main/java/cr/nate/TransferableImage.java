9d081970584e491bfcc47010aac4555a4724668a
DeflatedPickle <deflatedpickle@gmail.com>
2021-04-22 02:31:41 +0100

    Added TransferableImage.java

---
+++ src/main/java/cr/nate/TransferableImage.java
@@ -0,0 +1,44 @@
+package cr.nate;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+
+// https://coderanch.com/t/333565/java/BufferedImage-System-Clipboard
+public class TransferableImage implements Transferable {
+    Image i;
+
+    public TransferableImage(Image i) {
+        this.i = i;
+    }
+
+    @NotNull
+    public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException {
+        if (flavor.equals(DataFlavor.imageFlavor) && i != null) {
+            return i;
+        } else {
+            throw new UnsupportedFlavorException(flavor);
+        }
+    }
+
+    public DataFlavor[] getTransferDataFlavors() {
+        DataFlavor[] flavors = new DataFlavor[1];
+        flavors[0] = DataFlavor.imageFlavor;
+        return flavors;
+    }
+
+    public boolean isDataFlavorSupported(DataFlavor flavor) {
+        DataFlavor[] flavors = getTransferDataFlavors();
+
+        for (DataFlavor dataFlavor : flavors) {
+            if (flavor.equals(dataFlavor)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
