e8b937216122460ace3cd02b4198cdbfd7a9d05d
DeflatedPickle <deflatedpickle@gmail.com>
2021-05-03 22:45:55 +0100

    Added an extension to JTree to expand all nodes

---
+++ src/main/kotlin/com/deflatedpickle/undulation/extensions/JTree.kt
@@ -0,0 +1,16 @@
+package com.deflatedpickle.undulation.extensions
+
+import javax.swing.JTree
+
+
+fun JTree.expandAll() {
+    var j = this.rowCount
+    var i = 0
+    while (i < j) {
+        this.expandRow(i)
+        i += 1
+        // We keep expanding rows, increasing the row count
+        // So we need to keep updating it's value
+        j = this.rowCount
+    }
+}
