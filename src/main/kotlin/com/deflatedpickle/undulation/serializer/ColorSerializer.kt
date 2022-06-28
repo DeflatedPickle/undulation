2a0f3d98f868991b9b6637d0c9b5791ba2b9c8bf
DeflatedPickle <DeflatedPickle@gmail.com>
2022-06-28 18:32:04 +0100

    Added alpha values to color serializer and added a rectangle serializer

--- src/main/kotlin/com/deflatedpickle/undulation/serializer/ColorSerializer.kt
+++ src/main/kotlin/com/deflatedpickle/undulation/serializer/ColorSerializer.kt
@@ -3,14 +3,11 @@ package com.deflatedpickle.undulation.serializer
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
-import kotlinx.serialization.builtins.MapSerializer
-import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.awt.Color
-import java.awt.Point
 
 @ExperimentalSerializationApi
 @Serializer(forClass = Color::class)
@@ -21,7 +18,17 @@ object ColorSerializer : KSerializer<Color> {
     )
 
     override fun serialize(encoder: Encoder, value: Color) =
-        encoder.encodeString(String.format("#%06x", value.rgb and 0xFFFFFF))
+        encoder.encodeString(
+            String.format(
+                "#%08x",
+                (value.alpha and 0xFF shl 24) or (value.red and 0xFF shl 16) or
+                        (value.green and 0xFF shl 8) or (value.blue and 0xFF shl 0)
+            )
+        )
 
-    override fun deserialize(decoder: Decoder): Color = Color.decode(decoder.decodeString())
+    override fun deserialize(decoder: Decoder): Color {
+        val intval = java.lang.Long.decode(decoder.decodeString())
+        val i = intval.toInt()
+        return Color(i shr 16 and 0xFF, i shr 8 and 0xFF, i and 0xFF, i shr 24 and 0xFF)
+    }
 }
---
+++ src/main/kotlin/com/deflatedpickle/undulation/serializer/RectangleSerializer.kt
@@ -0,0 +1,46 @@
+package com.deflatedpickle.undulation.serializer
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.builtins.MapSerializer
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.awt.Rectangle
+
+@ExperimentalSerializationApi
+@Serializer(forClass = Rectangle::class)
+object RectangleSerializer : KSerializer<Rectangle> {
+    override val descriptor = PrimitiveSerialDescriptor(
+        serialName = "Rectangle",
+        kind = PrimitiveKind.STRING,
+    )
+
+    override fun serialize(encoder: Encoder, value: Rectangle) =
+        encoder.encodeSerializableValue(
+            MapSerializer(String.serializer(), Int.serializer()),
+            mapOf(
+                "x" to value.x,
+                "y" to value.y,
+                "width" to value.width,
+                "height" to value.height,
+            )
+        )
+
+    override fun deserialize(decoder: Decoder): Rectangle {
+        val decode = decoder.decodeSerializableValue(
+            MapSerializer(String.serializer(), Int.serializer()),
+        )
+
+        return Rectangle(
+            decode["x"] ?: 0,
+            decode["y"] ?: 0,
+            decode["width"] ?: 0,
+            decode["height"] ?: 0,
+        )
+    }
+}
